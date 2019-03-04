b58ccdc30e6478d65cd6395c0c9fa3771111b9e9
Varun Garg <varun.10@live.com>
2019-03-04 16:51:36 +0530

    Selenium 4 changes (#14)
    * java 10 and selenium 3
    
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>
    
    * selenium 4 changes
    
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>
    
    * code review changes
    
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>
    
    * fix local
    
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Varun-garg <varun.10@live.com>
    
    * bump local version
    
    * throw exception in case of exception
    
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Varun-garg <varun.10@live.com>
    
    * Update src/test/resources/conf/local.conf.json
    
    Co-Authored-By: Varun-garg <varun.10@live.com>
    
    * Update src/test/resources/conf/single.conf.json
    
    Co-Authored-By: Varun-garg <varun.10@live.com>

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -1,39 +1,29 @@
 package com.browserstack;
-import com.browserstack.local.Local;
 
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
+import java.util.Map;
+
+import com.browserstack.local.Local;
+
 import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Factory;
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 
 
 public class BrowserStackTestNGTest {
     public WebDriver driver;
-    private Local l;
+    private Local bsLocal;
 
     @BeforeMethod(alwaysRun=true)
     @org.testng.annotations.Parameters(value={"config", "environment"})
+    @SuppressWarnings("unchecked")
     public void setUp(String config_file, String environment) throws Exception {
         JSONParser parser = new JSONParser();
         JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
@@ -41,20 +31,23 @@ public class BrowserStackTestNGTest {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
-        Iterator it = envCapabilities.entrySet().iterator();
+        Map<String, Object> envCapabilities = (Map<String, Object>) envs.get(environment);
+        Iterator<Map.Entry<String, Object>> it = envCapabilities.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+            Map.Entry<String, Object> pair = (Map.Entry<String, Object>) it.next();
+            capabilities.setCapability(pair.getKey().toString(), pair.getValue());
         }
-        
-        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+
+        Map<String, Object> commonCapabilities = (Map<String, Object>) config.get("capabilities");
         it = commonCapabilities.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            if(capabilities.getCapability(pair.getKey().toString()) == null){
-                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+            Map.Entry<String, Object> pair = (Map.Entry<String, Object>) it.next();
+            Object envData = capabilities.getCapability(pair.getKey().toString());
+            Object resultData = pair.getValue();
+            if (envData != null && envData.getClass() == JSONObject.class) {
+                ((JSONObject) resultData).putAll((JSONObject) envData);
             }
+            capabilities.setCapability(pair.getKey().toString(), resultData);
         }
 
         String username = System.getenv("BROWSERSTACK_USERNAME");
@@ -67,19 +60,36 @@ public class BrowserStackTestNGTest {
             accessKey = (String) config.get("key");
         }
 
-        if(capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local") == "true"){
-            l = new Local();
+        this.checkAndStartBrowserStackLocal(capabilities, accessKey);
+
+        driver = new RemoteWebDriver(new URL("https://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
+    }
+
+    public void checkAndStartBrowserStackLocal(DesiredCapabilities capabilities, String accessKey) throws Exception {
+        if (bsLocal != null) {
+            return;
+        }
+        if (capabilities.getCapability("bstack:options") != null
+                && ((JSONObject) capabilities.getCapability("bstack:options")).get("local") != null
+                && ((Boolean) ((JSONObject) capabilities.getCapability("bstack:options")).get("local")) == true) {
+            bsLocal = new Local();
             Map<String, String> options = new HashMap<String, String>();
             options.put("key", accessKey);
-            l.start(options);
+            try {
+                bsLocal.start(options);
+            } catch (Exception e) {
+                System.out.println("Error: could not start browserstack local");
+                e.printStackTrace();
+                throw e;
+            }
         }
-
-        driver = new RemoteWebDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
     }
 
     @AfterMethod(alwaysRun=true)
     public void tearDown() throws Exception {
         driver.quit();
-        if(l != null) l.stop();
+        if (bsLocal != null) {
+            bsLocal.stop();
+        }
     }
 }
